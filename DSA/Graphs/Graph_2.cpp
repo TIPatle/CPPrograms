3f817c6e4ded43fa789968a80b3291a0ba297cf7
= <tipatle940@gamil.com>
2022-04-30 18:28:17 +0530

    New Program added

---
+++ DSA/Graphs/Graph_2.cpp
@@ -0,0 +1,22 @@
+// BFS Graph Implentation :- another way
+
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+queue <int> q;
+
+void bfsTravesal( ){
+
+    
+
+}
+
+int main(){
+
+
+
+    return 0;
+
+}
